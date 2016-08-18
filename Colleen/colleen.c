682e58f6d28fa035bb824a3f01f724a0c89e6333
tiboitel <tiboitel@student.42.fr>
2016-08-18 20:29:31 +0200

    Add Colleen exercise

---
+++ Colleen/colleen.c
@@ -0,0 +1,3 @@
+#include<stdio.h>
+/* Ceci est un commentaire */
+void ft_void(void){return;}int main(void){/*Ceci est un commentaire*/char*a="#include<stdio.h>%c/* Ceci est un commentaire */%cvoid ft_void(void){return;}int main(void){/*Ceci est un commentaire*/char*a=%c%s%c;printf(a,10,10,34,a,34,10);}%c";printf(a,10,10,34,a,34,10);}
