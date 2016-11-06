78829311373edc026489944fe51fb397655fd565
Timothee BOITELLE <tiboitel@e1r11p6.42.fr>
2016-11-06 22:48:50 +0100

    Add a lamba useless function to Colleen.

--- Bonus/Colleen.py
+++ Bonus/Colleen.py
@@ -1,7 +1,10 @@
+def nothing():
+	pass
+
 def main():
 	# THIS IS COMMENT
-	s = "def main():{:s}# THIS IS COMMENT{:s}s = {:s}print(s.format((chr(10) + chr(9)), (chr(10) + chr(9)), (chr(34) + s + chr(34) + chr(10) + chr(9)), (chr(10) + chr(10)), chr(10))){:s}main(){:s}# THIS IS SECOND COMMENT"
-	print(s.format((chr(10) + chr(9)), (chr(10) + chr(9)), (chr(34) + s + chr(34) + chr(10) + chr(9)), (chr(10) + chr(10)), chr(10)))
+	s = "def nothing():{:s}pass{:s}def main():{:s}# THIS IS COMMENT{:s}s = {:s}print(s.format((chr(10) + chr(9)), (chr(10) + chr(10)), (chr(10) + chr(9)), (chr(10) + chr(9)), (chr(34) + s + chr(34) + chr(10) + chr(9)), (chr(10) + chr(10)), chr(10))){:s}main(){:s}# THIS IS SECOND COMMENT"
+	print(s.format((chr(10) + chr(9)), (chr(10) + chr(10)), (chr(10) + chr(9)), (chr(10) + chr(9)), (chr(34) + s + chr(34) + chr(10) + chr(9)), (chr(10) + chr(10)), chr(10)))
 
 main()
 # THIS IS SECOND COMMENT
